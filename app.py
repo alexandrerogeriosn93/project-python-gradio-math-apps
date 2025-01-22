68e4c0bff9547e647d1512b0ed970ca75e8efc88
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-01-22 11:19:35 -0300

    feat: create interface to calc factorial

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 from functions import calc_factorial, calc_primorial, convert_temperature
+import gradio as gr
+
+with gr.Blocks() as iface:
+    with gr.Accordion("Fatorial"):
+        input_number = gr.Number(label="Digite um número para calcular o fatorial")
+        output_number = gr.Number(label="Fatorial calculado")
+        gr.Button("Calcular").click(calc_factorial, input_number, output_number)
+
+iface.launch(share=False)
