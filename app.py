91682b19305373d06b57023b09ed0de085e15b59
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-01-22 11:31:15 -0300

    feat: create interface to convert temperature

--- app.py
+++ app.py
@@ -12,4 +12,14 @@ with gr.Blocks() as iface:
         output_number = gr.Number(label="Primorial calculado")
         gr.Button("Calcular").click(calc_primorial, input_number, output_number)
 
+    with gr.Accordion("Conversor de Temperatura"):
+        input_number = gr.Number(label="Digite a temperatura")
+        input_choice = gr.Radio(choices=["Celsius", "Fahrenheit"], label="Converter de")
+        output_number = gr.Number(label="Temperatura convertida")
+        gr.Button("Calcular").click(
+            convert_temperature,
+            inputs=[input_number, input_choice],
+            outputs=output_number,
+        )
+
 iface.launch(share=False)
